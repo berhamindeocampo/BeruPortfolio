d099d57b2826b3af40731c94a37cf6f137c9d716
berhamindeocampo <badeocampo28@gmail.com>
2026-06-15 17:10:19 +0800

    Fix: Move profile image to public folder with correct path

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,6 @@
   <section id="about" class="about-strip">
 
     <!-- LEFT: image/avatar side -->
-     <img src="./9d3201f4-588b-493a-beef-e1b172942115.jpg" alt="Berhamin de Ocampo">
     <div class="about-image-side">
       <div class="avatar-circle">
         <img
@@ -54,7 +53,7 @@ export default {
   name: 'About',
   data() {
     return {
-      profileImg: '/9d3201f4-588b-493a-beef-e1b172942115.jpg'
+      profileImg: './9d3201f4-588b-493a-beef-e1b172942115.jpg'
     }
   }
 }
