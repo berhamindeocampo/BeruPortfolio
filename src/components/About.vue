8f217588c4e38c890e2e9030db0e7e339955cee2
berhamindeocampo <badeocampo28@gmail.com>
2026-06-15 17:58:26 +0800

    added folder so it should be now working, right?

--- src/components/About.vue
+++ src/components/About.vue
@@ -53,7 +53,7 @@ export default {
   name: 'About',
   data() {
     return {
-      profileImg: '/beruchisa.jpg' // SDHKJFHKSDJBVKJSBVKJSBKJ, ITS BCS OF THE PERIOD THAT WEB DONT RECOGNIZE!!!!!!!!!! AND I TYPO'ED GIR COMMIT BLAH BLAH INSTEAD OF THE MF GIT
+      profileImg: 'img/beruchisa.jpg' // SDHKJFHKSDJBVKJSBVKJSBKJ, ITS BCS OF THE PERIOD THAT WEB DONT RECOGNIZE!!!!!!!!!! AND I TYPO'ED GIR COMMIT BLAH BLAH INSTEAD OF THE MF GIT
     }
   }
 }
