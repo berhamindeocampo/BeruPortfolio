fd9f146a3756579f01f3fa5060e3f1769dfae262
berhamindeocampo <badeocampo28@gmail.com>
2026-06-19 15:16:16 +0800

    Come dance with me in a rosy haze of yesterday.

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -39,7 +39,7 @@
               </div>
             </div>
           </div>
-          <a href="/Berhamin_de_Ocampo_Resume.pdf" download="Berhamin_de_Ocampo_Resume.pdf" class="download-btn">
+          <a href="/pdf/Berhamin_de_Ocampo_Resume.pdf" download="Berhamin_de_Ocampo_Resume.pdf" class="download-btn">
             Download Resume ↓
           </a>
         </div>
